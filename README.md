88799c81ff16c0b67239410fdc791c1c422f781c
Hosmell Warthon <hosmellwarthon@gmail.com>
2025-08-07 12:58:54 -0500

    Implementación funcionalidad del temporizador con botones de inicio pausa y reset

---
+++ README.md
@@ -0,0 +1,7 @@
+Hola!
+
+Este es mi primer proyecto utilizando las tecnologías HTML, CSS y JS.
+
+El objetivo de este proyecto es crear un temporizador pomodoro para uso personal. 
+
+Decidí realizar uno desde 0 y no utilizar otro uno existente ya que me pareció una buena idea como proyecto inicial pues estoy aprendiendo a programar.
--- script.js
+++ script.js
@@ -1,9 +1,79 @@
+let timeLeft = 25*60 // seconds 
+let timerInterval = null;
+let isRunning = false;
 
+function timeFormat (seconds) {
+    let minutes = Math.floor(seconds/60);
+    let remainingSeconds = seconds % 60;
 
+    let formattedMinutes = minutes.toString().padStart(2, "0");
+    let formattedSeconds = remainingSeconds.toString().padStart(2,"0");
+    return `${formattedMinutes}:${formattedSeconds}`
+}
 
-let changeUserButton = document.querySelector("#userButton");
+function updateDisplay () {
+    const timerDisplay = document.getElementById("timer")
+    timerDisplay.textContent = timeFormat (timeLeft)
+}
 
-let myHeading = document.querySelector("h1");
+function countDown () {
+    timeLeft--;
+    updateDisplay();
+
+    if (timeLeft <= 0 ) {
+        clearInterval(timerInterval);
+        isRunning = false;
+        alert("Toma un descanso")
+        resetTimer();
+    }
+}
+
+function startTimer () {
+    if (!isRunning) {
+        isRunning = true;
+        timerInterval = setInterval(countDown, 1000);
+
+        const button = document.getElementById("startButton")
+        button.textContent = "Pausar";
+    } else {
+        pauseTimer();
+    }
+}
+
+function pauseTimer () {
+    clearInterval(timerInterval);
+    isRunning = false;
+
+    let button = document.getElementById("startButton");
+    button.textContent = "Iniciar";
+}
+
+function resetTimer () {
+    clearInterval(timerInterval);
+    isRunning = false;
+    timeLeft = 25*60
+    updateDisplay();
+
+    let button = document.getElementById("resetButton")
+    button.textContent = "Reset";
+    
+    let buttonIniciarName = document.getElementById("startButton");
+    buttonIniciarName.textContent = "Iniciar"
+}
+
+document.addEventListener("DOMContentLoaded", function (){
+    updateDisplay();
+    let startPauseButton = document.getElementById ("startButton")
+    startPauseButton.addEventListener("click", startTimer)
+
+    let startResetButton = document.getElementById("resetButton")
+    startResetButton.addEventListener("click", resetTimer)
+
+})
+//
+const changeUserButton = document.querySelector("#userButton");
+
+const myHeading = document.querySelector("h1");
 
 function newUserName() {
     const newName = prompt("Ingresa tu nombre");
