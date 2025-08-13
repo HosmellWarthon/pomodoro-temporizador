01209c9b7bff431fd92eae786b5613c59f83be84
Hosmell Warthon <hosmellwarthon@gmail.com>
2025-08-13 14:24:30 -0500

    Cambio de formato del código con prettier

--- README.md
+++ README.md
@@ -2,6 +2,6 @@ Hola!
 
 Este es mi primer proyecto utilizando las tecnologías HTML, CSS y JS.
 
-El objetivo de este proyecto es crear un temporizador pomodoro para uso personal. 
+El objetivo de este proyecto es crear un temporizador pomodoro para uso personal.
 
 Decidí realizar uno desde 0 y no utilizar otro uno existente ya que me pareció una buena idea como proyecto inicial pues estoy aprendiendo a programar.
--- script.js
+++ script.js
@@ -1,97 +1,96 @@
-let timeLeft = 25*60 // seconds 
+let timeLeft = 25 * 60; // seconds
 let timerInterval = null;
 let isRunning = false;
 
-function timeFormat (seconds) {
-    let minutes = Math.floor(seconds/60);
-    let remainingSeconds = seconds % 60;
+function timeFormat(seconds) {
+  let minutes = Math.floor(seconds / 60);
+  let remainingSeconds = seconds % 60;
 
-    let formattedMinutes = minutes.toString().padStart(2, "0");
-    let formattedSeconds = remainingSeconds.toString().padStart(2,"0");
-    return `${formattedMinutes}:${formattedSeconds}`
+  let formattedMinutes = minutes.toString().padStart(2, "0");
+  let formattedSeconds = remainingSeconds.toString().padStart(2, "0");
+  return `${formattedMinutes}:${formattedSeconds}`;
 }
 
-function updateDisplay () {
-    const timerDisplay = document.getElementById("timer")
-    timerDisplay.textContent = timeFormat (timeLeft)
+function updateDisplay() {
+  const timerDisplay = document.getElementById("timer");
+  timerDisplay.textContent = timeFormat(timeLeft);
 }
 
-function countDown () {
-    timeLeft--;
-    updateDisplay();
+function countDown() {
+  timeLeft--;
+  updateDisplay();
 
-    if (timeLeft <= 0 ) {
-        clearInterval(timerInterval);
-        isRunning = false;
-        alert("Toma un descanso")
-        resetTimer();
-    }
-}
-
-function startTimer () {
-    if (!isRunning) {
-        isRunning = true;
-        timerInterval = setInterval(countDown, 1000);
-
-        const button = document.getElementById("startButton")
-        button.textContent = "Pausar";
-    } else {
-        pauseTimer();
-    }
-}
-
-function pauseTimer () {
+  if (timeLeft <= 0) {
     clearInterval(timerInterval);
     isRunning = false;
-
-    let button = document.getElementById("startButton");
-    button.textContent = "Iniciar";
+    alert("Toma un descanso");
+    resetTimer();
+  }
 }
 
-function resetTimer () {
-    clearInterval(timerInterval);
-    isRunning = false;
-    timeLeft = 25*60
-    updateDisplay();
+function startTimer() {
+  if (!isRunning) {
+    isRunning = true;
+    timerInterval = setInterval(countDown, 1000);
 
-    let button = document.getElementById("resetButton")
-    button.textContent = "Reset";
-    
-    let buttonIniciarName = document.getElementById("startButton");
-    buttonIniciarName.textContent = "Iniciar"
+    const button = document.getElementById("startButton");
+    button.textContent = "Pausar";
+  } else {
+    pauseTimer();
+  }
 }
 
-document.addEventListener("DOMContentLoaded", function (){
-    updateDisplay();
-    let startPauseButton = document.getElementById ("startButton")
-    startPauseButton.addEventListener("click", startTimer)
+function pauseTimer() {
+  clearInterval(timerInterval);
+  isRunning = false;
 
-    let startResetButton = document.getElementById("resetButton")
-    startResetButton.addEventListener("click", resetTimer)
+  let button = document.getElementById("startButton");
+  button.textContent = "Iniciar";
+}
 
-})
+function resetTimer() {
+  clearInterval(timerInterval);
+  isRunning = false;
+  timeLeft = 25 * 60;
+  updateDisplay();
+
+  let button = document.getElementById("resetButton");
+  button.textContent = "Reset";
+
+  let buttonIniciarName = document.getElementById("startButton");
+  buttonIniciarName.textContent = "Iniciar";
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+  updateDisplay();
+  let startPauseButton = document.getElementById("startButton");
+  startPauseButton.addEventListener("click", startTimer);
+
+  let startResetButton = document.getElementById("resetButton");
+  startResetButton.addEventListener("click", resetTimer);
+});
 //
 const changeUserButton = document.querySelector("#userButton");
 
 const myHeading = document.querySelector("h1");
 
 function newUserName() {
-    const newName = prompt("Ingresa tu nombre");
-    if (!newName) {
-        newUserName ();
-    }
-     else {localStorage.setItem("name", newName);
+  const newName = prompt("Ingresa tu nombre");
+  if (!newName) {
+    newUserName();
+  } else {
+    localStorage.setItem("name", newName);
     myHeading.textContent = `Bienvenido, ${newName}`;
-    }
+  }
 }
 
 if (!localStorage.getItem("name")) {
-    newUserName();
-}   else {
-    const storedName = localStorage.getItem("name");
-    myHeading.textContent = `Bienvenido, ${storedName}`;
+  newUserName();
+} else {
+  const storedName = localStorage.getItem("name");
+  myHeading.textContent = `Bienvenido, ${storedName}`;
 }
 
 changeUserButton.addEventListener("click", () => {
-    newUserName ();
+  newUserName();
 });
